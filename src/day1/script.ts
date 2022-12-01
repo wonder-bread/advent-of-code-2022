1350db0d27409e41f52b815b489e079bbd71f846
Alex M <alex.measday@ascellatech.com>
2022-12-01 14:58:05 -0500

    Day 1 solution

---
+++ src/day1/script.ts
@@ -0,0 +1,45 @@
+const { getInput } = require("../util");
+
+type FormattedAccumulatorType = {
+  count: number;
+  sums: number[];
+};
+
+const formatInput: Function = (input: string[]) =>
+  input.reduce(
+    (acc: FormattedAccumulatorType, line: string) => {
+      if (line === "") {
+        acc.sums.push(acc.count);
+        acc.count = 0;
+      } else {
+        acc.count += parseInt(line);
+      }
+      return acc;
+    },
+    {
+      count: 0,
+      sums: [],
+    }
+  ).sums;
+
+const solvePartOne: Function = (input: number[]) => Math.max(...input);
+
+const solvePartTwo: Function = (input: number[]) =>
+  input
+    .sort((a: number, b: number) => a - b)
+    .reverse()
+    .slice(0, 3)
+    .reduce((acc: number, calorieCount: number) => acc + calorieCount, 0);
+
+const main: Function = async () => {
+  const input: string[] = await getInput("../../src/day1/input.txt");
+  const formattedInput: number[] = formatInput(input);
+
+  const partOneSolution = solvePartOne(formattedInput);
+  const partTwoSolution = solvePartTwo(formattedInput);
+
+  console.log(`Solution for part one: ${partOneSolution}.`);
+  console.log(`Solution for part two: ${partTwoSolution}.`);
+};
+
+main();
